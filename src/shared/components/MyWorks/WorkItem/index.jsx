73287b7d213801bdf449fc31e6c4c93b5787f265
Michael Thinh <michaelthinh1008@gmail.com>
2024-07-22 18:13:18 +0700

    add: mt-global, t4cvs

--- src/shared/components/MyWorks/WorkItem/index.jsx
+++ src/shared/components/MyWorks/WorkItem/index.jsx
@@ -6,48 +6,62 @@ import Button from "@mui/material/Button";
 
 import { IconContext } from "react-icons";
 
-import { FaReact } from "react-icons/fa";
-
-export default function WorkItem() {
+export default function WorkItem({ project, iconSize }) {
     return (
         <motion.div
             className={classes.projectItem}
             whileHover={{
-                scale: 1.1,
-                transition: { duration: 0.3 },
+                scale: 1.05,
+                transition: { duration: 0.2 },
             }}
         >
             <div className={classes.projectBanner}>
-                <img src="/banner/project-banner-01.png" alt="Project banner" />
+                <img src={project.bannerImg} alt="Project banner" />
             </div>
             <div className={classes.projectInfo}>
                 <div className={classes.projectName}>
-                    <h3>MT Movies - Movie Searching and Details App</h3>
+                    <h3>{project.projectTitle}</h3>
                 </div>
                 <div className={classes.projectDescription}>
-                    <p>
-                        Movie App Website where you can see top movies and their
-                        descriptions, inspired from Netflix
-                    </p>
+                    <p>{project.projectDescription}</p>
                 </div>
                 <div className={classes.projectTech}>
-                    <IconContext.Provider
-                        value={{
-                            size: "64px",
-                            color: "#61dafb",
-                        }}
-                    >
-                        <div className={classes.techIcon} title="ReactJS">
-                            <FaReact />
-                        </div>
-                    </IconContext.Provider>
+                    {project.projectTechs &&
+                        project.projectTechs.map((tech) => (
+                            <IconContext.Provider
+                                key={tech.techTitle}
+                                value={{
+                                    size: `${iconSize}px`,
+                                    color: `${tech.iconColor}`,
+                                }}
+                            >
+                                <div
+                                    className={classes.techIcon}
+                                    title={tech.techTitle}
+                                >
+                                    <tech.techIcon />
+                                </div>
+                            </IconContext.Provider>
+                        ))}
                 </div>
                 <div className={classes.projectDate}>
-                    Latest modified: <span>Jun 9, 2023</span>
+                    Latest modified: <span>{project.projectDate}</span>
                 </div>
                 <div className={classes.projectCta}>
-                    <Button variant="outlined">Source Code</Button>
-                    <Button variant="outlined">Live Demo</Button>
+                    <Button
+                        variant="outlined"
+                        href={project.projectLink}
+                        target="_blank"
+                    >
+                        Source Code
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        href={project.projectDemo}
+                        target="_blank"
+                    >
+                        Live Demo
+                    </Button>
                 </div>
             </div>
         </motion.div>
--- src/shared/components/MyWorks/index.jsx
+++ src/shared/components/MyWorks/index.jsx
@@ -1,6 +1,88 @@
 import classes from "./styles.module.css";
 import WorkItem from "./WorkItem";
 
+import { FaReact, FaAmazon } from "react-icons/fa";
+import {
+    SiTailwindcss,
+    SiRedux,
+    SiTypescript,
+    SiNestjs,
+    SiPostgresql,
+} from "react-icons/si";
+
+const projects = [
+    {
+        bannerImg: "/banner/t4cvs-banner.png",
+        projectTitle: "t4CVs - Posting job vacancies and applying desired jobs",
+        projectDescription:
+            "A website for recruitments and Quick CV creation, inspired from TopCV.vn.",
+        projectTechs: [
+            {
+                techIcon: SiTypescript,
+                iconColor: "#2f74c0",
+                techTitle: "TypeScript",
+            },
+            {
+                techIcon: SiTailwindcss,
+                iconColor: "#19274b",
+                techTitle: "TailwindCSS",
+            },
+            {
+                techIcon: SiNestjs,
+                iconColor: "#e0234e",
+                techTitle: "NestJS",
+            },
+            {
+                techIcon: SiPostgresql,
+                iconColor: "#31648c",
+                techTitle: "PostgreSQL",
+            },
+            {
+                techIcon: FaAmazon,
+                iconColor: "#883022",
+                techTitle: "Amazon S3 Cloud Storage",
+            },
+        ],
+        projectDate: "Jun 9th, 2023",
+        projectLink: "https://github.com/michaelthinh/DACNPM",
+        projectDemo: "https://dacnpm-m9tw.onrender.com",
+    },
+    {
+        bannerImg: "/banner/mt-movies-banner.png",
+        projectTitle: "MT Movies - Movie Searching and Details App",
+        projectDescription:
+            "Movie App Website where you can see top movies and their descriptions, inspired from Netflix.",
+        projectTechs: [
+            { techIcon: FaReact, iconColor: "#61dafb", techTitle: "ReactJS" },
+        ],
+        projectDate: "Jun 9th, 2023",
+        projectLink: "https://github.com/michaelthinh/MT-Movies",
+        projectDemo: "https://mt-movies.vercel.app",
+    },
+    {
+        bannerImg: "/banner/mt-global-banner.png",
+        projectTitle: "MT Global - Detailed Countries App",
+        projectDescription:
+            "An app where you can search for countries around the world.",
+        projectTechs: [
+            { techIcon: FaReact, iconColor: "#61dafb", techTitle: "ReactJS" },
+            {
+                techIcon: SiTailwindcss,
+                iconColor: "#19274b",
+                techTitle: "TailwindCSS",
+            },
+            {
+                techIcon: SiRedux,
+                iconColor: "#7a50be",
+                techTitle: "Redux",
+            },
+        ],
+        projectDate: "Jun 6th, 2023",
+        projectLink: "https://github.com/michaelthinh/MT-Global",
+        projectDemo: "https://mt-global.vercel.app",
+    },
+];
+
 export default function MyWorks() {
     return (
         <div className={classes.aboutMe}>
@@ -9,7 +91,13 @@ export default function MyWorks() {
             </div>
             <div className={classes.context}>
                 <div className={classes.projects}>
-                    <WorkItem />
+                    {projects.map((project) => (
+                        <WorkItem
+                            key={project.projectTitle}
+                            project={project}
+                            iconSize={64}
+                        />
+                    ))}
                 </div>
             </div>
             <div className={classes.borderBottom}></div>
